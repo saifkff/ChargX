a2dbb6b2f097d059ca574ef14530bb221f8115bf
Saif <officialousaif@gmail.com>
2025-07-29 23:41:19 +0500

    s

--- verification-stuff/Temu/riscof_Temu.py
+++ verification-stuff/Temu/riscof_Temu.py
@@ -92,7 +92,7 @@ class Temu(pluginTemplate):
             test = testentry['test_path']
             test_dir = testentry['work_dir']
             elf = 'my.elf'
-            sig_file = os.path.join(test_dir, "file.signature")
+            sig_file = os.path.join(test_dir, self.name[:-1] + ".signature")
             compile_macros = ' -D' + " -D".join(testentry['macros'])
 
             cmd = self.compile_cmd.format(testentry['isa'].lower(), self.xlen, test, elf, compile_macros)
@@ -109,7 +109,7 @@ class Temu(pluginTemplate):
                     self.verilator_cmd.format(self.dut),
                     f'{self.dut}/obj_dir/Vcore',
                     f'cd -',
-                    f"cp {self.dut}/file.signature {testentry['work_dir']}/file.signature"
+                    f"cp {self.dut}/file.signature {os.path.join(test_dir, self.name[:-1] + ".signature")}"
                 ))
             else:
                 simcmd = 'echo "NO RUN"'
